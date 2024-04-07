357aa1e6295144d5312193b62d22b3a3abda89d4
M.ngugi <mngugi7@gmail.com>
2024-04-08 00:24:40 +0300

    Update Number_Theory.py

--- Number_Theory.py
+++ Number_Theory.py
@@ -1,2 +1,6 @@
-print(237 % 3)
-print(237 % 7)
+'''
+A snipet to check if the number 237 is
+divisible by 3 or 7.
+'''
+print("Yes it is divisible by 3 becaus ethe remainder is =",237 % 3)
+print("Not divisible by 7 because the remainder is = ",237 % 7)
