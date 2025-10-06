7adbd4e3f7c038ceba1a629d134e33608c2e9e07
mngugi <mngugi7@hotmail.com>
2025-10-06 22:19:56 +0300

    Update complex_numbers.py

--- complex_numbers.py
+++ complex_numbers.py
@@ -1,2 +1,55 @@
 a = complex(2, 3)
 print(a)
+
+b = 3 + 3j
+
+c = a + b
+print(c)    
+
+print(a.real)
+print(a.imag)
+
+print(b.real)
+print(b.imag)
+
+print(c.real)
+
+'''
+Complex Numbers
+Definition
+
+A complex number is any number that can be written in the form:
+
+z = a + bi
+
+where:
+
+a is the real part of z
+
+b is the imaginary part of z
+
+i is the imaginary unit, defined by i² = −1
+
+We write this as:
+
+Re(z) = a
+Im(z) = b
+
+Set of Complex Numbers
+
+The set of all complex numbers is denoted by:
+
+ℂ = { a + bi | a, b ∈ ℝ }
+
+Equality of Complex Numbers
+
+Two complex numbers are equal if and only if both their real and imaginary parts are equal.
+
+If z₁ = a + bi and z₂ = c + di, then:
+
+z₁ = z₂ ⟺ a = c and b = d
+
+Visual Intuition (Complex Plane)
+
+We can represent each complex number z = a + bi as a point (a, b) on the complex plane, also known as the Argand diagram.
+'''
