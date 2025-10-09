9d33725dce657e2a5b3cad90d7d1745fddae4aa4
mngugi <mngugi7@hotmail.com>
2025-10-09 12:50:23 +0300

    Update try_fastai_lib.py

--- try_fastai_lib.py
+++ try_fastai_lib.py
@@ -4,6 +4,8 @@ from fastai.collab import *
 from fastai.tabular.all import *
 path = untar_data(URLs.PETS)/'images'
 
+
+
 def is_cat(x): return x[0].isupper()
 dls = ImageDataLoaders.from_name_func(
     path, get_image_files(path), valid_pct=0.2, seed=42,
@@ -11,3 +13,6 @@ dls = ImageDataLoaders.from_name_func(
 
 learn = vision_learner(dls, resnet34, metrics=error_rate)
 learn.fine_tune(1)
+
+img = PILImage.create('images/cat.jpg')
+img.to_thumb()
